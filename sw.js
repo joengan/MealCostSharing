26d9467774a491d6010f503d1e3fb8232151ac43
Joe <ngan-123@hotmail.com>
2025-10-07 15:44:58 +0800

    嘗試加入可安裝格式

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'meal-cost-sharing-v1';
+const URLS_TO_CACHE = [
+  './MealCostSharing.html',
+  './manifest.json',
+  './sw.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
